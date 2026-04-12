760540e1cdac83058fb6261165afd99f280475e2
eppzx2 <elmirinfo351@gmail.com>
2026-04-12 13:15:25 +0400

    Bash script to determine how many requests the attacker has sent, where the attacker is identified as the IP address with the highest number of requests

---
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+log_file=${1:-logs.txt}
+attacker_ip=$(awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+awk -v ip="$attacker_ip" '$1==ip {count++} END {print count}' "$log_file"
