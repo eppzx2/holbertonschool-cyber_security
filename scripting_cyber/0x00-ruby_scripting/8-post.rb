6700906306ea20e6215467ce86ce9316675ae944
eppzx2 <elmirinfo351@gmail.com>
2026-05-18 06:26:52 -0400

    Ruby script that makes an HTTP POST request to a specified URL with optional body parameters and prints the response

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -19,5 +19,5 @@ def post_request(url, body_params)
 
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts JSON.parse(response.body).to_json
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
