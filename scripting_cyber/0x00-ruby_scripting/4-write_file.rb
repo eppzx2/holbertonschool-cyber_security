059413428b1d292f427c5ff69e5da5c9a107a150
eppzx2 <elmirinfo351@gmail.com>
2026-05-18 07:00:13 -0400

    Ruby function merge_json_files that merges JSON objects from one file into another

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -2,23 +2,18 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  data1 = JSON.parse(File.read(file1_path))
-  data2 = JSON.parse(File.read(file2_path))
+  data1 = File.exist?(file1_path) ? JSON.parse(File.read(file1_path)) : []
+  data2 = File.exist?(file2_path) ? JSON.parse(File.read(file2_path)) : []
 
-  merged = data1 + data2
+  merged_data = if data1.is_class == Array && data2.is_class == Array
+                  data2 + data1
+                elsif data1.is_a?(Hash) && data2.is_a?(Hash)
+                  data2.merge(data1)
+                else
+                  data2.is_a?(Array) ? data2 + Array(data1) : data1
+                end
 
-  File.write(file2_path, JSON.pretty_generate(merged))
-
-  puts "Merged JSON written to file.json"
-  puts "File merged successfully!"
-
-  counts = Hash.new(0)
-
-  merged.each do |item|
-    counts[item["userId"]] += 1
-  end
-
-  counts.sort.each do |id, count|
-    puts "#{id}: #{count}"
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_data))
   end
 end
