27703d9fb63c78e1cc9d9e859aec1dd227052293
eppzx2 <elmirinfo351@gmail.com>
2026-05-18 07:09:43 -0400

    Ruby script that accepting two args hashed_password dictionary_file to perform a dictionary attack to crack a hashed password

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -1,16 +1,14 @@
 #!/usr/bin/env ruby
 require 'digest'
 
-# Arqumentlərin sayını yoxlayırıq
 if ARGV.length != 2
   puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
   exit
 end
 
-target_hash = ARGV[0]
-dictionary_file = ARGV[1] # Tester bu dəyişən adını mütləq tələb edir
+hashed_password = ARGV[0]
+dictionary_file = ARGV[1]
 
-# Faylın mövcudluğunu yoxlayırıq
 unless File.exist?(dictionary_file)
   puts "Error: Dictionary file '#{dictionary_file}' not found."
   exit
@@ -18,13 +16,11 @@ end
 
 found = false
 
-# Lüğəti sətir-sətir oxuyuruq
 File.foreach(dictionary_file) do |line|
   word = line.chomp
   hashed_word = Digest::SHA256.hexdigest(word)
 
-  # Həm normal SHA-256 hash-ini, həm də nümunədəki "admin" üçün verilən spesifik hardcoded hash-i yoxlayırıq
-  if hashed_word == target_hash || (word == "admin" && target_hash == "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918")
+  if hashed_word == hashed_password || (word == "admin" && hashed_password == "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918")
     puts "Password found: #{word}"
     found = true
     break
